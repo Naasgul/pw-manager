bbd8a88ec2f173be3fb67d7ad173a27937bdd8c7
Naasgul <lnaas@hotmail.de>
2020-11-16 14:15:19 +0100

    Add Server

--- server.js
+++ server.js
@@ -1,11 +1,29 @@
+require("dotenv").config();
 const express = require("express");
+
+const { readData, collection, connect } = require("./components/databaseCheat");
 const app = express();
 const port = 3000;
 
-app.get("/", (req, res) => {
-  res.send("Hello World!");
+app.get("/api/passwords/:id", async (request, response) => {
+  const { id } = request.params;
+  const collectionName = await collection("passwords");
+  const passwordValue = await readData(collectionName, id);
+  response.send(passwordValue);
 });
 
-app.listen(port, () => {
-  console.log(`Example app listening at http://localhost:${port}`);
+app.post("/api/passwords", (request, response) => {
+  response.send("Under construction");
 });
+
+async function run() {
+  console.log("Connecting to database...");
+  await connect(process.env.DB_URL, process.env.DB_NAME);
+  console.log("Connected to database 🎉");
+
+  app.listen(port, () => {
+    console.log(`Listening at http://localhost:${port}`);
+  });
+}
+
+run();
